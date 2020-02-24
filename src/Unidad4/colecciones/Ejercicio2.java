e080a6a98df0c9ca3051c7599bc7ac9ac6ae0ec3
Nachofv <nachin.ifv@gmail.com>
2020-02-24 17:56:25 +0100

    Add files via upload

---
+++ src/Unidad4/colecciones/Ejercicio2.java
@@ -0,0 +1,29 @@
+package Unidad4.colecciones;
+
+import java.util.*;
+
+public class Ejercicio2 {
+
+
+		public static void main(String[] args) {
+			List<Integer> lista1 = new LinkedList<>();
+			for (int i=0; i<100; i++)
+				lista1.add((int) (Math.random() * 10000 + 1));
+			for (Integer n: lista1)
+				System.out.printf("%d  ", n);
+			System.out.println();
+			
+			Set<Integer> conjunto1 = new HashSet<>();
+			conjunto1.addAll(lista1);
+			Iterator<Integer> i = conjunto1.iterator();
+			while (i.hasNext())
+				System.out.printf("%d  ", i.next());
+			System.out.println();
+			
+			Set<Integer> conjunto2 = new TreeSet<>();
+			conjunto2.addAll(lista1);
+			conjunto2.forEach(a -> System.out.printf("%d  ", a));
+
+	}
+
+}
---
+++ src/Unidad4/colecciones/Ejercicio3.java
@@ -0,0 +1,42 @@
+package Unidad4.colecciones;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Scanner;
+import java.util.Set;
+import java.util.TreeSet;
+
+public class Ejercicio3 {
+
+	public static void main(String[] args) throws IOException {
+		
+		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+		
+		Set<String> repetidas = new TreeSet<>();
+		Set<String> noRepetidas = new TreeSet<>();
+		System.out.print("Introduce un texto: ");
+		String linea = in.readLine();
+		Scanner s = new Scanner(linea);
+		
+		while (s.hasNext()) {
+			String palabra = s.next();
+			
+			
+			if (!repetidas.contains(palabra)) {
+				if (!noRepetidas.add(palabra)) {
+					noRepetidas.remove(palabra);
+					repetidas.add(palabra);
+				}
+			}
+		}
+		
+		System.out.println("Repetidas: " + repetidas);
+		System.out.println("No repetidas: " + noRepetidas);
+		
+		
+		in.close();
+
+	}
+
+}
