45efc196b2d56cce74d3add2deb1ffe227694531
Nachofv <nachin.ifv@gmail.com>
2020-03-15 12:17:42 +0100

    Add files via upload
    Ejercicio de la unidad 3 resuelto

---
+++ Autor.java
@@ -0,0 +1,58 @@
+package Unidad5;
+
+public class Autor {
+	private String nombre;
+	private  String email;
+	private Genero genero;
+
+
+
+public Autor(String nombre,String email,Genero genero) {
+	this.nombre=nombre;
+	this.email=email;
+	this.genero=genero;
+	
+	}
+
+public String getNombre() {
+	return nombre;
+}
+
+
+public Genero getGenero() {
+	return genero;
+}
+
+public String getEmail() {
+	return email;
+}
+
+public void setEmail(String email) {
+	this.email = email;
+}
+
+
+
+public String toString() {
+	
+	return String.format("%s (%s) %s",nombre, genero.toString(),email);
+	
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
---
+++ PruebaAutor.java
@@ -0,0 +1,17 @@
+package Unidad5;
+
+
+
+public class PruebaAutor {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	
+		Autor autor= new Autor ("nacho fernandez","ifv@gemil.com", Genero.masculino);
+		
+		System.out.println(autor);
+			
+			
+	}
+
+}
