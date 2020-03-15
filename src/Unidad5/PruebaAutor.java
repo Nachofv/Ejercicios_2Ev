48ef820e7ec0e38cc13b06d10fe900d689e2c391
Usuario <Usuario@DESKTOP-4I7KDR3>
2020-03-15 17:44:29 +0100

    Ej3 ud5

---
+++ src/Unidad5/PruebaAutor.java
@@ -0,0 +1,16 @@
+package Unidad5;
+
+
+public class PruebaAutor {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	
+		Autor autor= new Autor ("nacho fernandez","ifv@gemil.com", Genero.masculino);
+		
+		System.out.println(autor);
+			
+			
+	}
+
+}
